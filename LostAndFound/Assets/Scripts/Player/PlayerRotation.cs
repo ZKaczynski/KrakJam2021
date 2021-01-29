91d189e3e52fce8ea71a14f7d39d95a267460823
ZKaczynski <zbigniew.kaczynski@op.pl>
2021-01-29 23:09:31 +0100

    Updated player

--- LostAndFound/Assets/Scripts/Player/PlayerRotation.cs
+++ LostAndFound/Assets/Scripts/Player/PlayerRotation.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerRotation : MonoBehaviour
-{
-
-
-    // Update is called once per frame
-    void Update()
+namespace Player { 
+    public class PlayerRotation : MonoBehaviour
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 
-        Vector3 perpendicular = Vector3.Cross(transform.position - mousePos, Vector3.forward);
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, perpendicular);
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+            Vector3 perpendicular = Vector3.Cross(transform.position - mousePos, Vector3.forward);
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, perpendicular);
+        }
     }
 }
