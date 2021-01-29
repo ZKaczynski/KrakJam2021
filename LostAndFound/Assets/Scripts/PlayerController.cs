68a467b6781c1912e570f1bc94d056576d8aaa98
ZKaczynski <zbigniew.kaczynski@op.pl>
2021-01-29 20:04:10 +0100

    Simple player

---
+++ LostAndFound/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+
+    public float speed = 10;
+
+    void Update() {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed * inputX, speed * inputY,0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
