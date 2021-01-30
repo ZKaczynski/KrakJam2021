a9a2c7aaeb2de46e8da4ba5532717068be65d103
ZKaczynski <zbigniew.kaczynski@op.pl>
2021-01-30 23:56:57 +0100

    Better Flare

--- LostAndFound/Assets/Scripts/LevelMechanics/Flare.cs
+++ LostAndFound/Assets/Scripts/LevelMechanics/Flare.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Flare : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Light2D light2D;
+
+    public float lifetime = 10f;
+    public float flickeringIntensivity = 0.01f;
+
     void Start()
     {
-        
+        light2D = GetComponent<Light2D>();
+        StartCoroutine(FlareLife());
+        StartCoroutine(Flicker());
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+    }
+
+    IEnumerator Flicker()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.value/10);
+            if (Random.value > 0.5)
+            {
+                light2D.intensity = light2D.intensity - flickeringIntensivity;
+            }
+            else
+            {
+                light2D.intensity = light2D.intensity + flickeringIntensivity;
+            }
+        }
+
+
+    }
+
+    IEnumerator FlareLife()
+    {
+
+        yield return new WaitForSeconds(lifetime);
+
+        while (light2D.intensity > 0.3)
+        {
+            yield return new WaitForSeconds(Random.value / 20);
+            light2D.intensity -= flickeringIntensivity;
+            
+        }
+
+        Destroy(gameObject);
         
+
     }
 }
