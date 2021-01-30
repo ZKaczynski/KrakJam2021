ddd998904a17000592b8c775ff55518532d9f906
ZKaczynski <zbigniew.kaczynski@op.pl>
2021-01-30 04:37:20 +0100

    Fov initial

---
+++ LostAndFound/Assets/Scripts/Player/Fov.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fov : MonoBehaviour{
+
+    private Mesh mesh;
+    [SerializeField]
+    float viewDistance = 5f;
+    [SerializeField]
+    float fov = 90f;
+    [SerializeField]
+    int rayCount = 50;
+
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    Vector3 origin;
+    float startingAngle;
+
+
+    void Start() {
+
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        origin = Vector3.zero;
+    }
+
+
+    void LateUpdate() { 
+
+        float angle = startingAngle;
+        float angleIncrease = fov / rayCount;
+
+
+        Vector3[] vertices = new Vector3[rayCount+1+1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount*3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex; 
+
+            RaycastHit2D rayCast = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+            if (rayCast.collider == null)
+            {
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+
+                Debug.DrawLine(origin, vertex, Color.blue);
+            }
+            else
+            {
+                Debug.DrawLine(origin, rayCast.point,Color.red);
+                vertex = rayCast.point;
+            }
+            
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+                triangleIndex += 3;
+            }
+            angle -= angleIncrease;
+            vertexIndex++;
+            
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
+
+
+    }
+
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+
+    public void SetOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection)
+    {
+        this.startingAngle = GetAngleFromVectorFloat(aimDirection) - fov - 45;
+    }
+
+    public static float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+}
--- LostAndFound/Assets/Scripts/Player/PlayerRotation.cs
+++ LostAndFound/Assets/Scripts/Player/PlayerRotation.cs
@@ -6,14 +6,23 @@ namespace Player {
     public class PlayerRotation : MonoBehaviour
     {
 
+        [SerializeField]
+        private Fov fov;
 
         // Update is called once per frame
         void Update()
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+            fov.SetAimDirection((transform.position - mousePos).normalized);
+            //print(( mousePos- transform.position).normalized);
 
             Vector3 perpendicular = Vector3.Cross(transform.position - mousePos, Vector3.forward);
+           fov.SetOrigin(transform.position);
+            
+
+
+
             transform.rotation = Quaternion.LookRotation(Vector3.forward, perpendicular);
         }
     }
