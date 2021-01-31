8128fc27d7bca7c74dc1debd478d6118610514bd
Szymon <sszymek.5@gmail.com>
2021-02-01 00:25:52 +0100

    Fixed enemy movement

--- LostAndFound/Assets/Scripts/Enemy/EnemyMovement.cs
+++ LostAndFound/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,4 +1,6 @@
-﻿using LevelMechanics;
+﻿using System.Collections.Generic;
+using System.Linq;
+using LevelMechanics;
 using UnityEngine;
 
 namespace Enemy
@@ -14,14 +16,12 @@ namespace Enemy
 
         private Transform target;
         private Vector3? lastPosition;
-        private bool lastPositionHasMeaningfulValue;
-
         private AudioSource audio = null;
 
         private bool ShouldMove => target != null || lastPosition.HasValue;
         private bool CanMove => InLight == false;
 
-        private int lightsCount = 0;
+        private List<EnemyTarget> lightSourcesInRange = new List<EnemyTarget>();
 
         void Start()
         {
@@ -37,6 +37,8 @@ namespace Enemy
                 return;
             }
 
+            UpdateInLightStatus();
+
             if (ShouldMove && CanMove)
             {
                 if (!audio.isPlaying) {
@@ -46,36 +48,10 @@ namespace Enemy
                 if (target != null && HasLineOfSight(target, Color.clear))
                 {
                     lastPosition = target.position;
-
-                    Vector3 objectPos = transform.position;
-
+                    
                     Vector3 dir = (Vector3)(lastPosition - transform.position);
-
-                    //targ.x = targ.x - objectPos.x;
-                    //targ.y = targ.y - objectPos.y;
-
                     float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
                     transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
-                    /*
-                    Vector3 vectorToTarget = (Vector3)(lastPosition - transform.position);
-                    float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-                    Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-                    transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
-                    */
-
-                    //transform.LookAt(target.position);
-
-
-                    //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                    //Vector2 perpendicular = Vector3.Cross((Vector3)(transform.position - lastPosition), Vector3.forward);
-                    //transform.rotation = Quaternion.LookRotation(Vector3.forward, perpendicular);
-
-                    /*
-                    float dotProduct = Vector3.Dot(Vector2.up, (transform.position - target.position).normalized);
-                    float multiplier = dotProduct < 0 ? 1 : -1;
-                    spriteTransform.rotation = Quaternion.Euler(0, 0, multiplier * Vector2.Angle(Vector2.up, (transform.position - target.position).normalized));*/
                 }
 
                 if (lastPosition.HasValue)
@@ -92,27 +68,33 @@ namespace Enemy
             }
             
         }
+
+        private void UpdateInLightStatus()
+        {
+            lightSourcesInRange.RemoveAll(light => light == null);
+            
+            EnemyTarget potentialTarget = lightSourcesInRange.FirstOrDefault(target => HasLineOfSight(target.getTarget(), Color.clear));
+            if (potentialTarget != null)
+            {
+                InLight = true;
+                target = potentialTarget.getTarget();
+            }
+            else
+            {
+                InLight = false;
+            }
+        }
         
         private void OnTriggerEnter2D(Collider2D other)
-        {
-            HasEnteredLight(other);
-        }
-
-        private void HasEnteredLight(Collider2D other)
         {
             EnemyTarget potentialTarget = other.gameObject.GetComponent<EnemyTarget>();
 
             if (potentialTarget != null)
             {
-                if (HasLineOfSight(potentialTarget.getTarget(), Color.red))
-                {
-                    lightsCount++;
-                    InLight = true;
-                    target = potentialTarget.getTarget();
-                }
+                lightSourcesInRange.Add(potentialTarget);
             }
         }
-
+        
         private void OnTriggerStay2D(Collider2D other)
         {
             if (GameMaster.CanTrapsKillEnemies)
@@ -131,15 +113,7 @@ namespace Enemy
 
             if (potentialTarget != null)
             {
-                if (HasLineOfSight(potentialTarget.getTarget(), Color.green))
-                {
-                    target = potentialTarget.getTarget();
-                    lightsCount--;
-                    if (lightsCount == 0)
-                    {
-                        InLight = false;
-                    }
-                }
+                lightSourcesInRange.Remove(potentialTarget);
             }
         }
 
